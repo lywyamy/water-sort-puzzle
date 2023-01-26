714e6c2c3fd11a3378600e78fab4de8be06d96b8
Yangweiyi Li <lywy.amy@gmail.com>
2023-01-26 14:27:21 -0800

    add BottleController, last commit before re-installing vscode.

---
+++ Assets/Scripts/BottleController.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BottleController : MonoBehaviour
+{
+    public Color[] bottleColors;
+    public SpriteRenderer bottleMaskSR;
+
+    public AnimationCurve ScaleAndRotationMultiplierCurve;
+    public AnimationCurve FillRateCurve;
+
+    void Start()
+    {
+        UpdateColorsOnShader();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.P))
+        {
+            StartCoroutine(RotateBottle());
+        }
+    }
+
+    void UpdateColorsOnShader()
+    {
+        bottleMaskSR.material.SetColor("_Color1", bottleColors[0]);
+        bottleMaskSR.material.SetColor("_Color2", bottleColors[1]);
+        bottleMaskSR.material.SetColor("_Color3", bottleColors[2]);
+        bottleMaskSR.material.SetColor("_Color4", bottleColors[3]);
+    }
+
+    public float timeToRotate = 1.0f;
+    IEnumerator RotateBottle()
+    {
+        float t = 0;
+        float lerpValue;
+        float angleValue;
+
+        while (t < timeToRotate)
+        {
+            lerpValue = t / timeToRotate;
+            angleValue = Mathf.Lerp(0.0f, 90.0f, lerpValue);
+
+            transform.eulerAngles = new Vector3(0, 0, angleValue);
+            bottleMaskSR.material.SetFloat("_ScaleAndRotationMultiplier", ScaleAndRotationMultiplierCurve.Evaluate(angleValue));
+
+            t += Time.deltaTime;
+
+            yield return new WaitForEndOfFrame();
+        }
+        angleValue = 90.0f;
+        transform.eulerAngles = new Vector3(0, 0, angleValue);
+        bottleMaskSR.material.SetFloat("_ScaleAndRotationMultiplier", ScaleAndRotationMultiplierCurve.Evaluate(angleValue));
+    }
+}
