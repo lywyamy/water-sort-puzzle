afb7cb68b208c0a709cc1d3c551bdf0c0feeaf13
Yangweiyi Li <lywy.amy@gmail.com>
2023-01-31 13:41:57 -0800

    implement clicking to pour

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public BottleController FirstBottle;
+    public BottleController SecondBottle;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector2 mousePosition2D = new Vector2(mousePosition.x, mousePosition.y);
+
+            RaycastHit2D hit = Physics2D.Raycast(mousePosition2D, Vector2.zero);
+
+            if (hit.collider != null)
+            {
+                if (hit.collider.GetComponent<BottleController>() != null)
+                {
+                    if (FirstBottle == null)
+                    {
+                        FirstBottle = hit.collider.GetComponent<BottleController>();
+                    }
+                    else
+                    {
+                        if (FirstBottle == hit.collider.GetComponent<BottleController>())
+                        {
+                            FirstBottle = null;
+                        }
+                        else
+                        {
+                            SecondBottle = hit.collider.GetComponent<BottleController>();
+                            FirstBottle.BottleControllerRef = SecondBottle;
+
+                            FirstBottle.UpdateTopColorValues();
+                            SecondBottle.UpdateTopColorValues();
+
+                            if (SecondBottle.FillBottleCheck(FirstBottle.topColor))
+                            {
+                                FirstBottle.StartColorTransfer();
+                            }
+
+                            FirstBottle = null;
+                            SecondBottle = null;
+                        }
+                    }
+                }
+            }
+        }
+
+    }
+}
