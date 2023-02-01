f52fd38a6a97b578b82e886d200a7b70a875d4c8
Yangweiyi Li <lywy.amy@gmail.com>
2023-02-01 15:39:42 -0800

    implement auto bottle creation and color filling for easy level

--- Assets/Scripts/BottleSpawner.cs
+++ Assets/Scripts/BottleSpawner.cs
@@ -6,12 +6,14 @@ public class BottleSpawner : MonoBehaviour
 
 {
     public GameObject bottlePrefab;
-    public GameObject[] bottles;
+    // public GameObject[] bottles;
     public BottleController bottleController;
-    public System.Random random = new System.Random();
+    public System.Random random = new System.Random(Constants.SEED + 1);
+    public Color[] colors;
 
     void Start()
     {
+        populateColors();
         generateBottles(Constants.EASY_NUMBER_OF_BOTTLES);
     }
 
@@ -22,10 +24,18 @@ public class BottleSpawner : MonoBehaviour
 
     void generateBottles(int numberOfFullBottles)
     {
+        Color[] colorPool = generateColorPool(colors, numberOfFullBottles);
+        int index = 0;
+
         for (int i = 0; i < numberOfFullBottles; i++)
         {
-            GameObject bottle = Instantiate(bottlePrefab);
-            bottle.transform.position = new Vector3(-12 + i * 2, 0, 0);
+            GameObject bottleObject = Instantiate(bottlePrefab);
+            bottleObject.transform.position = new Vector3(-12 + i * 2, 0, 0);
+
+            BottleController bottle = bottleObject.GetComponent<BottleController>();
+            bottle.numberOfColorsInBottle = 4;
+
+            bottle.bottleColors = new Color[] { colorPool[index++], colorPool[index++], colorPool[index++], colorPool[index++] };
         }
 
         for (int i = 0; i < Constants.NUMBER_OF_EMPTY_BOTTLES; i++)
@@ -35,5 +45,33 @@ public class BottleSpawner : MonoBehaviour
             BottleController bottleController = bottle.GetComponent<BottleController>();
             bottleController.numberOfColorsInBottle = 0;
         }
+    }
+
+    private Color[] generateColorPool(Color[] colors, int numberOfFullBottles)
+    {
+        Color[] colorPool = new Color[Constants.MAX_NUMBER_OF_COLORS_IN_BOTTLE * numberOfFullBottles];
+        int index = 0;
+
+        for (int i = 0; i < numberOfFullBottles; i++)
+        {
+            for (int j = 0; j < Constants.MAX_NUMBER_OF_COLORS_IN_BOTTLE; j++)
+            {
+                colorPool[index++] = colors[i];
+            }
+        }
+
+        //Shuffle the colors
+        for (int i = 0; i < colorPool.Length - 1; i++)
+        {
+            int r = random.Next(i, colorPool.Length);
+            (colorPool[r], colorPool[i]) = (colorPool[i], colorPool[r]);
+        }
+
+        return colorPool;
+    }
+
+    private void populateColors()
+    {
+        colors = new Color[] { Constants.RED, Constants.ORANGE, Constants.PINK, Constants.TEAL, Constants.SKYBLUE, Constants.PURPLE, Constants.NAVYBLUE, Constants.GRAY, Constants.GREEN, Constants.YELLOW, Constants.DARKGREEN, Constants.BROWN };
     }
 }
--- Assets/Scripts/Constants.cs
+++ Assets/Scripts/Constants.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class Constants : MonoBehaviour
 {
@@ -8,24 +9,24 @@ public class Constants : MonoBehaviour
     public const int MEDIUM_NUMBER_OF_BOTTLES = 9;
     public const int HARD_NUMBER_OF_BOTTLES = 12;
     public const int NUMBER_OF_EMPTY_BOTTLES = 2;
+    public const int MAX_NUMBER_OF_COLORS_IN_BOTTLE = 4;
+    public const int SEED = 3722;
 
     // Basic colors for easy level
-    public static Color red;
-    public static Color orange;
-    public static Color pink;
-    public static Color teal;
-    public static Color skyBlue;
-    public static Color purple;
+    public static Color RED = new Color(0.796f, 0.192f, 0.165f); // CB312A
+    public static Color ORANGE = new Color(0.918f, 0.58f, 0.294f); // EA944B
+    public static Color PINK = new Color(0.922f, 0.396f, 0.502f); // EB6580
+    public static Color TEAL = new Color(0.412f, 0.855f, 0.514f); // 69DA83
+    public static Color SKYBLUE = new Color(0.349f, 0.659f, 0.902f); // 59A8E6
+    public static Color PURPLE = new Color(0.482f, 0.184f, 0.608f); // 7B2F9B
 
     // Additional colors for medium level
-    public static Color navyBlue;
-    public static Color gray;
-    public static Color green;
+    public static Color NAVYBLUE = new Color(0.267f, 0.208f, 0.792f); // 4435CA
+    public static Color GRAY = new Color(0.416f, 0.431f, 0.443f); // 6A6E71
+    public static Color GREEN = new Color(0.498f, 0.612f, 0.071f); // 7F9C12
 
     // Aditional colors for hard level
-    public static Color yellow;
-    public static Color darkGreen;
-    public static Color brown;
-
-    public Color[] colors = { red, orange, pink, teal, skyBlue, purple, navyBlue, gray, green, yellow, darkGreen, brown };
+    public static Color YELLOW = new Color(0.949f, 0.867f, 0.376f); // F2DD60
+    public static Color DARKGREEN = new Color(0.09f, 0.431f, 0.239f); // 176E3D
+    public static Color BROWN = new Color(0.525f, 0.325f, 0.035f); // 865309
 }
